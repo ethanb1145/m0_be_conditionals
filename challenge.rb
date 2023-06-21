72bfdeef1788bd52e15b51b1a3f47f3b9df7bc5d
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-06-21 14:57:17 -0600

    Complete challenge

---
+++ challenge.rb
@@ -0,0 +1,10 @@
+good_driving_record = true 
+age = 28
+
+if good_driving_record == true && age >= 25
+    puts "You will get a discount"
+elsif good_driving_record == true || age >= 25
+    puts "You will pay full price"
+else 
+    puts "You will need a cosigner"
+end
